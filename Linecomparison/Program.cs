04aef51c0b4f893a5f46562b0239d1d3f329ee66
gitakshay1 <akshaypatil309p8@gmail.com>
2022-12-01 23:36:48 +0530

    [Akshay]-Calculating Length of Line

---
+++ Linecomparison/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace LineComparison
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter x1-cordinate :");
+            int x1 = Convert.ToInt16(Console.ReadLine());
+
+            Console.WriteLine("Enter y1-cordinate :");
+            int y1 = Convert.ToInt16(Console.ReadLine());
+
+            Console.WriteLine("Enter x2-cordinate :");
+            int x2 = Convert.ToInt16(Console.ReadLine());
+
+            Console.WriteLine("Enter y2-cordinate :");
+            int y2 = Convert.ToInt16(Console.ReadLine());
+
+            Double Length = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+            Console.WriteLine(Length);
+        }
+    }
+}
