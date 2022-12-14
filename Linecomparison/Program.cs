8a2ed74c16d9d5b7cbc7b34f0c2aeabc8f82bdd6
gitakshay1 <akshaypatil309p8@gmail.com>
2022-12-14 20:20:13 +0530

    [Akshay]-Using Class

--- Linecomparison/Program.cs
+++ Linecomparison/Program.cs
@@ -4,49 +4,74 @@ namespace LineComparison
 {
     class Program
     {
-        static void Main(string[] args)
+        public int len1, len2, x1, x2, y1, y2;
+        public Program()
         {
-            Double L1 = 0, L2 = 0;
-            for(int i = 0; i <= 1; i++)
+            double[] StoringLength = new double[5];
+            for (int i = 1; i < 2; i++)
             {
-                Console.WriteLine("Enter x1-cordinate :");
-                int x1 = Convert.ToInt16(Console.ReadLine());
+                Console.WriteLine("Enter value for  coordinate x1");
+                this.x1 = Convert.ToInt32(Console.ReadLine());
 
-                Console.WriteLine("Enter y1-cordinate :");
-                int y1 = Convert.ToInt16(Console.ReadLine());
+                Console.WriteLine("Enter value for   coordinate y1");
+                this.y1 = Convert.ToInt32(Console.ReadLine());
 
-                Console.WriteLine("Enter x2-cordinate :");
-                int x2 = Convert.ToInt16(Console.ReadLine());
 
-                Console.WriteLine("Enter y2-cordinate :");
-                int y2 = Convert.ToInt16(Console.ReadLine());
+                Console.WriteLine("Enter value for  first coordinate x2");
+                this.x2 = Convert.ToInt32(Console.ReadLine());
 
-                Double Length = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
-                if (i == 0)
-                {
-                    Console.WriteLine("Length of First Line :" + Length);
-                     L1 = Length;
-                }
-                else 
-                {
-                    Console.WriteLine("Length of Second Line :" + Length);
-                     L2 = Length;
-                }
+                Console.WriteLine("Enter value for  first coordinate y2");
+                this.y2 = Convert.ToInt32(Console.ReadLine());
             }
-            if(L1 == L2)
+        }
+        public double FindingLength()
+        {
+
+            double LineLength = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+            double len = Math.Round(LineLength, 2);
+            return len;
+
+        }
+
+        public void EqualityMethod(string len1, string len2)
+        {
+            if (len1.Equals(len2))
             {
-                Console.WriteLine("Lines are Equal");
-            }
-            else if(L1 > L2)
-            {
-                Console.WriteLine("First Line is greater than second");
+                Console.WriteLine("Lines are equal");
             }
             else
             {
-                Console.WriteLine("First line is less than second");
+                Console.WriteLine("Lines are not equal");
             }
+        }
 
-            
+        public void comparingLines(string len1, string len2)
+        {
+            if (len1.CompareTo(len2) > 0)
+            {
+                Console.WriteLine("Line-1 is greater than line-2");
+            }
+            else if (len1.CompareTo(len2) < 0)
+            {
+                Console.WriteLine("Line-1 is lesser than line-2");
+            }
+            if (len1.CompareTo(len2) > 0)
+            {
+                Console.WriteLine("both lines are equal");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Program obj1 = new Program();
+            double length1 = obj1.FindingLength();
+            Program obj2 = new Program();
+            double length2 = obj2.FindingLength();
+
+            Console.WriteLine("Length of First Line " + length1);
+            Console.WriteLine("Length of Second Line " + length2);
+            obj1.EqualityMethod(length1.ToString(), length2.ToString());
+            obj2.comparingLines(length1.ToString(), length2.ToString());
+
         }
     }
 }
